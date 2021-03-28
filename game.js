de448083d6ba34a2ab57744fe74b0ba8a6a24444
PaulinaPawelczyk <paulinapatrycja.fior@gmail.com>
2021-03-28 18:45:55 +0200

    Add default values for game

--- game.js
+++ game.js
@@ -1,25 +1,33 @@
 class Game {
-    constructor() {
+    constructor(moneyAtstart) {
         this.gameResult = new GameResult();
-        this.wallet = new Wallet(1000); // money for start
+        this.wallet = new Wallet(moneyAtstart); // money for start
 
         const button = document.getElementById('play_start');
         button.addEventListener('click', this.playGame());
 
-        this.fruitsResult = document.querySelectorAll('div.fruit');
+        this.fruitsResult = [...document.querySelectorAll('div.fruit')];
         this.inputAmount = document.getElementById('amount');
-        this.wallet = document.querySelector('.amount_panel span.wallet');
-        this.gameResult = document.querySelector('.game .game_result');
-        this.gameNumber = document.querySelector('.game .game_number');
-        this.gameWins = document.querySelector('.game .game_win');
-        this.gameLoses = document.querySelector('.game .game_loss');
+        this.walletSpan = document.querySelector('.amount_panel span.wallet');
+        this.gameResult = document.querySelector('.games .game_result');
+        this.gameNumber = document.querySelector('.games .game_number');
+        this.gameWins = document.querySelector('.games .game_win');
+        this.gameLoses = document.querySelector('.games .game_loss');
 
         this.init();
 
     }
 
-    init() {
-        console.log("plaay");
+    init(money = this.wallet.getActualValue(), gameResult = '', result = [0, 0, 0], fruits = ['star', 'star', 'star']) {
+        this.walletSpan.textContent = money;
+        this.gameResult.textContent = gameResult;
+        this.gameNumber.textContent = result[0];
+        this.gameWins.textContent = result[1];
+        this.gameLoses.textContent = result[2];
+
+        this.fruitsResult.forEach((fruit, index) => {
+            fruit.style.backgroundImage = `url(img/${fruits[index]}.png)`;
+        })
 
     }
 
